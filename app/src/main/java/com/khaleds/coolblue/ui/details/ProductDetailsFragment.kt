929ded2011a70122394a0074032bdd3294b5c1ad
Mustafa Khaled Mustafa Mohamed <mustafakhaled290@gmail.com>
2020-07-05 15:29:58 +0200

    added 2 fragments, Home and details Added their xml

---
+++ app/src/main/java/com/khaleds/coolblue/ui/details/ProductDetailsFragment.kt
@@ -0,0 +1,29 @@
+package com.khaleds.coolblue.ui.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.khaleds.coolblue.R
+
+class ProductDetailsFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_product_details,container,false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/khaleds/coolblue/ui/home/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.khaleds.coolblue.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.khaleds.coolblue.R
+
+class HomeFragment: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home,container,false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+}
