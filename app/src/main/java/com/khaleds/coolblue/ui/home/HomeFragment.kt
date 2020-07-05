53739faae4822465d64b75afd589e7a77a104737
Mustafa Khaled Mustafa Mohamed <mustafakhaled290@gmail.com>
2020-07-05 23:10:03 +0200

    added progressbar for all products fragment

--- app/src/main/java/com/khaleds/coolblue/ui/home/HomeFragment.kt
+++ app/src/main/java/com/khaleds/coolblue/ui/home/HomeFragment.kt
@@ -5,16 +5,40 @@ import android.view.*
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.khaleds.coolblue.MainActivity
 import com.khaleds.coolblue.R
+import com.khaleds.coolblue.data.di.components.DaggerDataComponent
+import com.khaleds.coolblue.presentation.di.component.DaggerPresentationComponent
+import com.khaleds.coolblue.presentation.factory.ViewModelFactory
 import com.khaleds.coolblue.presentation.viewmodels.AllProductsViewModel
+import com.khaleds.coolblue.ui.home.di.component.DaggerAllProductsComponent
+import com.khaleds.coolblue.util.MyApplication
+import com.khaleds.coolblue.util.StateUi
+import kotlinx.android.synthetic.main.fragment_home.*
+import javax.inject.Inject
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
     lateinit var allProductsViewModel: AllProductsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+        DaggerAllProductsComponent.builder()
+            .presentationComponent(
+                DaggerPresentationComponent.builder()
+                    .dataComponent(
+                        DaggerDataComponent.builder()
+                            .applicationComponent(MyApplication.applicationComponent).build()
+                    )
+                    .build()
+            ).build().inject(this)
 
+        allProductsViewModel =
+            ViewModelProvider(this, viewModelFactory).get(AllProductsViewModel::class.java)
+        allProductsViewModel.getAllProducts()
     }
 
     override fun onCreateView(
@@ -22,12 +46,32 @@ class HomeFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home,container,false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        allProductsViewModel.observeState().observe(viewLifecycleOwner, Observer {
+            when (it) {
+                is StateUi.Loading -> {
+                    progressBar.visibility = View.VISIBLE
+                }
+
+                is StateUi.Success -> {
+                    progressBar.visibility = View.GONE
+
+                }
+
+                is StateUi.Error -> {
+                    progressBar.visibility = View.GONE
+
+                }
+
+            }
+
+        })
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
--- app/src/main/java/com/khaleds/coolblue/ui/home/di/component/AllProductsComponent.kt
+++ app/src/main/java/com/khaleds/coolblue/ui/home/di/component/AllProductsComponent.kt
@@ -2,8 +2,9 @@ package com.khaleds.coolblue.ui.home.di.component
 
 import com.khaleds.coolblue.presentation.di.component.PresentationComponent
 import com.khaleds.coolblue.ui.home.HomeFragment
+import com.khaleds.coolblue.ui.home.di.scope.AllProductsScope
 import dagger.Component
-
+@AllProductsScope
 @Component(dependencies = [PresentationComponent::class])
 interface AllProductsComponent {
     fun inject(homeFragment: HomeFragment) {
