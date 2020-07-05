0f62345301f4d5c247566875508328291f2a33f5
Mustafa Khaled Mustafa Mohamed <mustafakhaled290@gmail.com>
2020-07-05 16:25:56 +0200

    added search item to home fragment

--- app/src/main/java/com/khaleds/coolblue/ui/home/HomeFragment.kt
+++ app/src/main/java/com/khaleds/coolblue/ui/home/HomeFragment.kt
@@ -1,15 +1,18 @@
 package com.khaleds.coolblue.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.khaleds.coolblue.MainActivity
 import com.khaleds.coolblue.R
 
 class HomeFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+//        (activity as AppCompatActivity?)!!.supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onCreateView(
@@ -23,6 +26,31 @@ class HomeFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+        inflater.inflate(R.menu.search_menu, menu)
+        val searchView =
+            SearchView((context as MainActivity).supportActionBar?.themedContext ?: context)
+        searchView.isIconified = false
+        menu.findItem(R.id.action_search).apply {
+            setShowAsAction(MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW or MenuItem.SHOW_AS_ACTION_IF_ROOM)
+            actionView = searchView
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(query: String): Boolean {
+
+                return true
+            }
+        })
+        searchView.setOnClickListener { view -> }
     }
 
 
