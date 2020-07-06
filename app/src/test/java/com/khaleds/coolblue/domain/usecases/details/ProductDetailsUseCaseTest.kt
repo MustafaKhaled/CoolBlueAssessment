d10355a4316cff442ac2407fa6a2dd239e22389b
Mustafa Khaled Mustafa Mohamed <mustafakhaled290@gmail.com>
2020-07-06 18:28:08 +0200

    Add ProductDetailsUseCase test file

---
+++ app/src/test/java/com/khaleds/coolblue/domain/usecases/details/ProductDetailsUseCaseTest.kt
@@ -0,0 +1,12 @@
+package com.khaleds.coolblue.domain.usecases.details
+
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class ProductDetailsUseCaseTest {
+
+    @Before
+    fun setUp() {
+    }
+}
