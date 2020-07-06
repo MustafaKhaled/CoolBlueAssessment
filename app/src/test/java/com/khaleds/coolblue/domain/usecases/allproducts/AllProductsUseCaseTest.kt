d174f11e8833ab0a9daf1f970ba0d7e69835c65c
Mustafa Khaled Mustafa Mohamed <mustafakhaled290@gmail.com>
2020-07-06 18:26:16 +0200

    unit tests for All Products usecase

--- app/src/test/java/com/khaleds/coolblue/domain/usecases/allproducts/AllProductsUseCaseTest.kt
+++ app/src/test/java/com/khaleds/coolblue/domain/usecases/allproducts/AllProductsUseCaseTest.kt
@@ -4,13 +4,12 @@ import com.khaleds.coolblue.data.remote.entities.ProductDetailsResponse
 import com.khaleds.coolblue.data.remote.entities.ProductsResponse
 import com.khaleds.coolblue.data.repo.ProductsRepositoryImpl
 import com.khaleds.coolblue.domain.repo.ProductsRepository
+import com.khaleds.coolblue.domain.usecases.details.ProductDetailsUseCase
 import com.khaleds.coolblue.util.TestCoroutineRule
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.times
-import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
+import com.nhaarman.mockitokotlin2.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -24,6 +23,8 @@ class AllProductsUseCaseTest {
     @get:Rule
     val testCoroutineRule = TestCoroutineRule()
     private lateinit var useCase: AllProductsUseCase
+    @Mock
+    private lateinit var detailsUseCase: ProductDetailsUseCase
 
     @Mock
     private lateinit var productsRepository: ProductsRepository
@@ -34,6 +35,7 @@ class AllProductsUseCaseTest {
     fun setup(){
         MockitoAnnotations.initMocks(this)
         useCase = AllProductsUseCase(productsRepository)
+//        detailsUseCase = ProductDetailsUseCase(productsRepository)
     }
 
     @Test
@@ -43,6 +45,24 @@ class AllProductsUseCaseTest {
             val result = useCase.getAllProducts()
             verify(productsRepository, times(1)).getAllProducts()
             assertEquals(result,productsResponse)
+        }
+    }
+
+    @Test
+    fun `call getProducts() and verify no interaction for details with success`(){
+        testCoroutineRule.runBlockingTest {
+            `when`(productsRepository.getAllProducts()).thenReturn(productsResponse)
+            useCase.getAllProducts()
+            verifyZeroInteractions(detailsUseCase)
+        }
+    }
+
+    @Test(expected = java.lang.RuntimeException::class)
+    fun `execute ProductDetails by Id and return null if exception thrown`() {
+        runBlockingTest {
+            whenever(productsRepository.getAllProducts()).thenThrow(RuntimeException::class.java)
+            useCase.getAllProducts()
+            assertNull(useCase.getAllProducts())
         }
     }
 }
